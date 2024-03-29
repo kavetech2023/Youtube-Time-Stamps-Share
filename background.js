a33d795381934a549114c3d6e770499b16018862
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-03-29 23:12:59 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
+const sendMessageToContentScript = (tabId, url) => {
+  if (url && url.includes("youtube.com/watch")) {
+    const queryParameters = url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    }, function(response) {
+      if (chrome.runtime.lastError) {
+        console.log(`Error: ${chrome.runtime.lastError.message}`);
+      } else {
+        console.log(`Received response: ${response}`);
+      }
+    });
+  }
+};
+
   
