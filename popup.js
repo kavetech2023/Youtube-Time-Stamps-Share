f8baaf62cedc96ebc5e02258414bbd66744a45bf
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-04-15 23:27:35 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
-
+// function to view bookmarks
 const viewBookmarks = () => {};
-
+//function to play youtube video
 const onPlay = e => {};
 
 const onDelete = e => {};
